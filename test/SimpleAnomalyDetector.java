0d509f6be96edb09cb62b37c46ae8fc3d6633039
gal levy <gallevy34@cs.colman.ac.il>
2021-12-06 14:11:56 +0200

    fixed

--- test/SimpleAnomalyDetector.java
+++ test/SimpleAnomalyDetector.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SimpleAnomalyDetector  implements TimeSeriesAnomalyDetector {
-	public List<CorrelatedFeatures> correlatedFeaturesList = new ArrayList<CorrelatedFeatures>();;
+	public List<CorrelatedFeatures> correlatedFeaturesList = new ArrayList<CorrelatedFeatures>();
 public static final double CORRELATION_THRESHOLD =  0.9;
 	public List<AnomalyReport> anomalyReports = new ArrayList<AnomalyReport>();
 
@@ -51,13 +51,14 @@ public static final double CORRELATION_THRESHOLD =  0.9;
 
 	@Override
 	public List<AnomalyReport> detect(TimeSeries ts) {
-		int timeStep = 0;
 		for(CorrelatedFeatures c:this.correlatedFeaturesList){
+			int timeStep = 0;
 			float[] floatArray1 = new float[ts.columns.get(c.feature1).size()];
 			 floatArray1 = ts.stringToFloat(ts.columns.get(c.feature1));
 
 			float[] floatArray2 = new float[ts.columns.get(c.feature2).size()];
 			 floatArray2 = ts.stringToFloat(ts.columns.get(c.feature2));
+
 			Point[] myPoints = new Point[floatArray1.length];
 			for(int j = 0; j < myPoints.length ; j++){
 				myPoints[j] = new Point(floatArray1[j] , floatArray2[j]);
@@ -70,7 +71,9 @@ public static final double CORRELATION_THRESHOLD =  0.9;
 					AnomalyReport tempReport = new AnomalyReport(c.feature1+ "-" +
 							c.feature2, timeStep );
 					anomalyReports.add(tempReport);
+					timeStep++;
 				}
+
 			}
 		}
 
